7f405471b4e14db3bce68147addd1fd092189ba3
Andres Vargas <andreselias.vargas@gmail.com>
2021-12-07 17:36:43 -0600

    chatUI
    Created a Class for the ChatUI

--- js/app.js
+++ js/app.js
@@ -1,2 +1,11 @@
 'use strict';
 
+// DOM queries
+const chatList = document.querySelector('.chat-list');
+
+// class instances
+const chatUI = new ChatUI(chatList);
+const chatRoom = new Chatroom('general', 'Andrés');
+
+// get chats and render
+chatRoom.getChats(data => { chatUI.render(data) });
